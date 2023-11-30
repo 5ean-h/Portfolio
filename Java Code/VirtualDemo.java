3cee51d4828d237d7b0dd522b18a0a5ff6d049b7
Sean <144811089+5ean-h@users.noreply.github.com>
2023-11-30 13:37:40 +0000

    Add files via upload

---
+++ Java Code/VirtualDemo.java
@@ -0,0 +1,17 @@
+/*
+Name: Sean Hogan
+Function: creating test class for employeedir
+cd
+Creation Date: 22/11/2022
+*/
+public class VirtualDemo {
+
+   public static void main(String [] args) {
+      Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
+      System.out.println("Call mailCheck using Salary reference --");   
+      s.mailCheck();
+      System.out.println("\n Call mailCheck using Employee reference--");
+      e.mailCheck();
+   }
+}
